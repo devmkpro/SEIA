4628ac5c59f824b4191c1a241bcaccac209db870
josevictoremiliano <josevictoremiliano@gmail.com>
2024-01-09 21:25:16 -0300

    Update tooltips for admin and secretary nav items data-bs-placement="right"

--- resources/views/layouts/partials/nav-li-itens-for-secretary.blade.php
+++ resources/views/layouts/partials/nav-li-itens-for-secretary.blade.php
@@ -1,5 +1,5 @@
 <span class="menu-title align-self-start">Secretaria</span>
-<li class="nav-item" data-bs-toggle="tooltip" data-bs-title="Gestão">
+<li class="nav-item" data-bs-toggle="tooltip" data-bs-title="Gestão" data-bs-placement="right">
     <a class="nav-link dropdown-toggle red center " href="#" id="navbarDropdownLocations" 
         role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="pph ph-graduation-cap icons-menu"></i>
