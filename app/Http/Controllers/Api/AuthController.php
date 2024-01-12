dc5b0f89142583c84094c7436a069d474b674064
devmaike <155475483+devmaike@users.noreply.github.com>
2024-01-12 14:57:46 -0300

    Refactor authentication and add CookieController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -17,19 +17,15 @@ class AuthController extends Controller
         if (Auth::attempt($request->only('email', 'password'))) {
             $user = User::where('email', $request->email)->first();
             if (!$user->active) {
-                return response()->json([
-                    'message' => 'Usuário inativo',
-                ], 401);
-            } else {
-                return response()->json([
-                    'access_token' => $user->createToken('invoice')->plainTextToken,
-                    'token_type' => 'Bearer',
-                ]);
+                return response()->json(['message' => 'Usuário inativo'], 401);
             }
+            
+            return response()->json([
+                'access_token' => $user->createToken('invoice')->plainTextToken,
+                'token_type' => 'Bearer',
+            ]);
         }
 
-        return response()->json([
-            'message' => 'Login inválido',
-        ], 401);
+        return response()->json(['message' => 'Login inválido'], 401);
     }
 }
---
+++ app/Http/Controllers/CookieController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class CookieController extends Controller
+{
+    /**
+     * Set Cookie in browser
+     */
+    public function setCookie($name, $value, $minutes = 60)
+    {
+        Cookie::queue($name, $value, $minutes);
+        return;
+    }
+
+    /**
+     * Delete Cookie in browser
+     */
+    public function deleteCookie($name)
+    {
+        Cookie::queue(Cookie::forget($name));
+        return;
+    }
+
+}
